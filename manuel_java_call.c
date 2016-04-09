e5f959bb89ad4d85e10c2a13b2caed78d5025d66
cagdasyelen <cagdas@utexas.edu>
2016-04-09 17:14:25 -0500

    Example java Hello class added

--- manuel_java_call.c
+++ manuel_java_call.c
@@ -12,10 +12,11 @@ int main(int argc, char *argv[]){
    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
    MPI_Get_processor_name(name,  &namelen);
    char c1[4][200]; 
-   strcpy(c1[0], "java -cp build:lib/junit.jar:lib/javassist.jar:lib/commons-cli-1.0.jar:lib/alloy4viz.jar korat.Korat --class korat.examples.binarytree.BinaryTree --args 2 2 2 --cvStart 0 --cvEnd 4"); 
-   strcpy(c1[1], "java -cp build:lib/junit.jar:lib/javassist.jar:lib/commons-cli-1.0.jar:lib/alloy4viz.jar korat.Korat --class korat.examples.binarytree.BinaryTree --args 2 2 2 --cvStart 4 --cvEnd 8");
-   strcpy(c1[2], "java -cp build:lib/junit.jar:lib/javassist.jar:lib/commons-cli-1.0.jar:lib/alloy4viz.jar korat.Korat --class korat.examples.binarytree.BinaryTree --args 2 2 2 --cvStart 8 --cvEnd 12");
-   strcpy(c1[3], "java -cp build:lib/junit.jar:lib/javassist.jar:lib/commons-cli-1.0.jar:lib/alloy4viz.jar korat.Korat --class korat.examples.binarytree.BinaryTree --args 2 2 2 --cvStart 12");
+   strcpy(c1[0], "java Hello"); 
+   strcpy(c1[1], "java Hello"); 
+   strcpy(c1[2], "java Hello"); 
+   strcpy(c1[3], "java Hello"); 
+   
    if(rank%8 == 0){
      system(c1[rank/8]); 
    }
